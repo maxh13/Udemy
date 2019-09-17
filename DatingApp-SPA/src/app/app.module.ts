0fa82660e2d4637f6c4a2add658753c5fe82ff8d
0933248 <0933248@hr.nl>
2019-09-17 09:39:06 +0200

    initial commit

---
+++ DatingApp-SPA/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ValueComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
